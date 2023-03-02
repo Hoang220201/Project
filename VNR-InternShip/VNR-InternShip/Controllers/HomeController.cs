2518e89a947aae04e5326d46cdede2644b4ebca6
hoang220201 <hoang22201@gmail.com>
2023-03-02 13:46:21 +0700

    VNR-InternShip

---
+++ VNR-InternShip/VNR-InternShip/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VNR_InternShip.Controllers
+{
+    public class HomeController : Controller
+    {
+        private VNR_DBDataContext context = new VNR_DBDataContext();
+
+        public ActionResult Index()
+        {
+            var Courses = context.KhoaHocs.ToList();
+            return View(Courses);
+        }
+
+        public ActionResult Subject(int id)
+        {
+            var Subject = context.MonHocs.ToList().FindAll(m => m.KhoaHocID == id);
+            ViewBag.CourseName = context.KhoaHocs.ToList().Find(m => m.ID == id);
+            return View(Subject);
+        }
+
+    }
+}
---
+++ VNR-InternShip/VNR-InternShip/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VNR_InternShip.MvcApplication" Language="C#" %>
